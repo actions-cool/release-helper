34129cb5b1fd94b55179048cfe64cd4fb3da219b
xrkffgg <xrkffgg@gmail.com>
2021-10-09 11:11:25 +0800

    fix: prettier not active (#8)
    * fix: prettier judge
    
    * t
    
    * fix
    
    * remove

--- dist/index.js
+++ dist/index.js
@@ -11371,7 +11371,7 @@ function main() {
             for (let i = 0; i < changelogArr.length; i += 1) {
                 // eslint-disable-next-line no-await-in-loop
                 const { data } = yield axios_1.default.get(`${url}/${changelogArr[i]}`);
-                const [changelog, changelogPre] = (0, util_1.getChangelog)(data, version, prettier === 'true');
+                const [changelog, changelogPre] = (0, util_1.getChangelog)(data, version, prettier !== '');
                 arr.push(changelog);
                 real.push(changelogPre);
                 if (changelog && i !== changelogArr.length - 1) {
@@ -11483,7 +11483,7 @@ const getChangelog = (content, version, prettier) => {
                     l = `${line.replace('-', '◆')}\n`;
                 if (line.startsWith('  -'))
                     l = `${line.replace('  -', '\xa0\xa0\xa0\xa0◇')}\n`;
-                l = `${line}\n`;
+                l = `${l}\n`;
             }
             changeLogPre.push(l);
             changeLog.push(line);
